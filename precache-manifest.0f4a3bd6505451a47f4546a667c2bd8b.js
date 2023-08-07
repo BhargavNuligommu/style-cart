4e2242a8d3a932ef97e2204038e6fe5ff5ca6b74
BhargavNuligommu <nuligommu.bhargav@gmail.com>
2023-08-07 09:40:05 -0400

    Updates

---
+++ precache-manifest.0f4a3bd6505451a47f4546a667c2bd8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6931d230b2070d123ad523256b499ba9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "834bdcd4f4de202dc764",
+    "url": "/static/css/main.829d0e9b.chunk.css"
+  },
+  {
+    "revision": "cbe38a34c01dafdb89da",
+    "url": "/static/js/2.b690a798.chunk.js"
+  },
+  {
+    "revision": "834bdcd4f4de202dc764",
+    "url": "/static/js/main.d7f60e0c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
